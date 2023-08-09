c28a934ed1524ccd0b9db59af22dd56218407e39
Fran Perez <fran06.pe@gmail.com>
2023-08-09 02:06:13 -0500

    loading env files before pkg initializes

--- cmd/crud-api/main.go
+++ cmd/crud-api/main.go
@@ -4,13 +4,22 @@ import (
 	"github.com/FranP0610/go-crud/pkg/database"
 	"github.com/FranP0610/go-crud/pkg/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
 )
 
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error loading .env file")
+	}
+}
 func main() {
 	database.Connection()
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 	http.ListenAndServe(":8080", r)
-
+	//r := gin.Default()
+	//r.GET()
 }
--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -13,25 +12,13 @@ var DB *gorm.DB
 
 func getDbConnectionChain() string {
 	var connectionChain string
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("error loading .env file")
-	} else {
-		connectionChain = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_NAME"),
-			os.Getenv("DB_PORT"))
-	}
-	//connectionChain = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
-	//	os.Getenv("DB_HOST"),
-	//	os.Getenv("DB_USER"),
-	//	os.Getenv("DB_PASSWORD"),
-	//	os.Getenv("DB_NAME"),
-	//	os.Getenv("DB_PORT"))
-	//log.Printf("db user is %s", os.Getenv("DB_USER"))
+	connectionChain = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+	log.Printf("db user is %s", os.Getenv("DB_USER"))
 	return connectionChain
 }
 
